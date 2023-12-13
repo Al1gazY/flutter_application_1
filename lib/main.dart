f93dbcf0fd10ed48ee897a2f9c106ce483cb1833
Al1gazY <al1gazy@mail.ru>
2023-12-13 17:27:15 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final List<String> _tasks = [];
+  final TextEditingController _textFieldController = TextEditingController();
+
+  void _addTask(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _tasks.add(task);
+      });
+      _textFieldController.clear();
+    }
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('To-Do List')),
+      body: ListView.builder(
+        itemCount: _tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_tasks[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _deleteTask(index),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayAddTaskDialog(context),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future<void> _displayAddTaskDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add a Task'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: "Enter task here"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('ADD'),
+              onPressed: () {
+                _addTask(_textFieldController.text);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+}
